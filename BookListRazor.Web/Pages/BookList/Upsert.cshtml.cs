1d4a44161c34cba961a8476d0991caf9ddcfd322
tiago-salsinha <tiagosalsinha@gmail.com>
2020-12-22 11:12:09 +0000

    Upsert

---
+++ BookListRazor.Web/Pages/BookList/Upsert.cshtml.cs
@@ -0,0 +1,62 @@
+﻿using BookListRazor.Web.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace BookListRazor.Web.Pages.BookList
+{
+    public class UpsertModel : PageModel
+    {
+        private DataContext _db;
+
+        public UpsertModel(DataContext db)
+        {
+            _db = db;
+        }
+
+
+        [BindProperty]
+        public Book Book { get; set; }
+
+
+        public async Task<IActionResult> OnGet(int? id)
+        {
+            Book = new Book();
+            if (id == null)
+            {
+                //create
+                return Page();
+            }
+
+            //update
+            Book = await _db.Book.FirstOrDefaultAsync(u => u.Id == id);
+            if(Book == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+
+        public async Task<IActionResult> OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                if (Book.Id == 0)
+                {
+                    _db.Book.Add(Book);
+                }
+                else
+                {
+                    _db.Book.Update(Book);
+                }
+
+                await _db.SaveChangesAsync();
+
+                return RedirectToPage("Index");
+            }
+            return RedirectToPage();
+        }
+    }
+}
